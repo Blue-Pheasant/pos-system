cb7489ae3514b8d2c1152050bdc3884451ff73a5
Blue-Pheasant <long@idctechno.com>
2022-06-29 22:58:19 +0700

    Add cart table

---
+++ database/migrations/2022_06_28_074950_create_cart_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->string('size');
+            $table->string('quantity');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('carts', function (Blueprint $table) {
+            $table->unsignedBigInteger('cart_id');
+            $table->foreign('cart_id')->references('id')->on('carts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
